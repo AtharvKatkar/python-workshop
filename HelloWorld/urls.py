f71779993442505d6b4dba7b6426c7bf7c0a6907
AtharvKatkar <atharvkatkar47@gmail.com>
2025-08-30 11:15:23 +0530

    Added math module

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -22,4 +22,5 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',  views.learn_python),
+    path('math/',  views.learn_math),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views he e
 def learn_python(request):
     return HttpResponse("<h1>Hello world</h1>")
+
+def learn_math(request):
+    a = 10
+    b = 20
+    c = a + b
+    return HttpResponse(f"<h1>Answer: {c}</h1>")
